01f090e20f5239255ec1f2a0672538902138d8f9
liuweiseu <liuwei_seu@seu.edu.cn>
2021-01-12 13:36:54 +0800

    update adc_snapshot_inside.py, adding dac initialization code.

--- adc_4x16g_test/fpg_bit_files/adc_snapshot_inside.py
+++ adc_4x16g_test/fpg_bit_files/adc_snapshot_inside.py
@@ -9,4 +9,9 @@ import casperfpga
 fpga=casperfpga.CasperFpga('10.0.1.25')
 fpga.get_system_information('adc_snapshot_inside_2021-01-10_2322.fpg')
 fpga.adcs.adc_16g_asnt.adc_init()
+reg_addr = [1,3,5,7,2,4,6,8]
+# These vals are related to the specific adc board
+# Different boards should have different val lists
+val=[825,775,700,513,420,410,410,410]
+fpga.adcs.adc_16g_asnt.set_DACs(reg_addr,val)
 fpga.adcs.adc_16g_asnt.set_alignment()
